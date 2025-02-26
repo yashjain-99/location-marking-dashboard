83735cc16a62d3e0086dab09799c779c7c2b5743
yashjain-99 <yashj133.yj@gmail.com>
2025-02-26 20:13:35 +0530

    init - Setup docker-compose for postgres, node & keycloak

---
+++ api/src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = process.env.API_PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`API listening on port ${API_PORT}`);
+});
